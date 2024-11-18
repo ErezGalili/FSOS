6b98cf4f383f554550bd172336270f31ab702cf8
ErezGalili <galiliere13@gmail.com>
2024-11-18 12:36:54 +0200

    18.11 update 2

--- simon game/Simon Game Challenge Starting Files/game.js
+++ simon game/Simon Game Challenge Starting Files/game.js
@@ -1,8 +1,9 @@
-const buttonColours = ["red", "blue","green","yellow"]
+const buttonColours = ["red", "blue", "green", "yellow"]
 const gamePattern = []
+const userClickedPattern = []
 
-function nextSequence(){
-    const randomNumber = Math.floor(Math.random()*4)
+function nextSequence() {
+    const randomNumber = Math.floor(Math.random() * 4)
     const randomChosenColour = buttonColours[randomNumber]
     gamePattern.push(randomChosenColour)
     $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100)
@@ -10,4 +11,7 @@ function nextSequence(){
     sound.play()
 }
 
-  
+$(".btn").on('click', function () {
+    const userChosenColour = $(this).attr("id")
+    userClickedPattern.push(userChosenColour)
+})
