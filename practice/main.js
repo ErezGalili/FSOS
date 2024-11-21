cc0551bd826f385246cdf041a93962de9acd96bb
ErezGalili <galiliere13@gmail.com>
2024-11-21 11:06:38 +0200

    21.11 update 1 Parentheses

--- practice/main.js
+++ practice/main.js
@@ -1,32 +1,25 @@
-const cont = document.querySelector(".cont");
-const ranNum = Math.ceil(Math.random() * 30);
+function isValidParentheses(s) {
+    const stack = []
+    const map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
 
-for (let i = 0; i < ranNum; i++) {
-    const el = document.createElement("div");
-    el.classList.add("circle");
-    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    el.style.backgroundColor = randomColor;
-    cont.appendChild(el);
+    const filteredString = s.split('').filter(char => '()[]{}'.includes(char))
+    for (let i = 0; i < filteredString.length; i++) {
+        const char = filteredString[i]
+
+        if (map[char]) {
+            stack.push(char)
+        } else {
+            const last = stack.pop()
+            if (map[last] !== char) {
+                return false
+            }
+        }
+    }
+    return stack.length === 0
 }
 
-const obj = {
-    score_a: 30,
-    score_b: 19,
-    score_c: 11,
-}
-
-for(let prop in obj) obj[prop]++;
-console.log(obj);
-
-const basArr = ['basmach', 'igul', 'class']
-const newBasArr = []
-for(let i = 0; i < 7; i++) newBasArr.push(...basArr);
-console.log(newBasArr)
-
-// Object.values(basArr).forEach(value => {
-//     for (let i = 0; i < 7; i++) {
-//         newBasArr.push(value);
-//     }
-// });
-// console.log(newBasArr);
-
+console.log(isValidParentheses("({[]})"));
