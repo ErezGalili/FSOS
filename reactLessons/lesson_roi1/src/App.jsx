d6a07a2dedfcf072dfe5f27f3f21e6288ae32c70
ErezGalili <galiliere13@gmail.com>
2024-12-17 09:55:40 +0200

    17.12 update 1

---
+++ reactLessons/lesson_roi1/src/App.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+import react, { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (<>
+
+    </>)
+}
+
+export default App
--- reactLessons/my_app/src/App.jsx
+++ reactLessons/my_app/src/App.jsx
@@ -26,6 +26,42 @@ function ColorSqure(props) {
     </>)
 }
 
+function SimpleQ(props) {
+  const [input, setInput] = useState("");
+  const [message, setMessage] = useState("Didn't answer yet");
+
+  const check = () => {
+      setMessage(input.toLowerCase() === props.a.toLowerCase() ? "Correct!" : "Incorrect");
+  }
+
+  return (    
+    <div>
+      <h3>{props.q}</h3>
+      <input type="text" placeholder="Your answer" value={input} onChange={e => setInput(e.target.value)} />
+      <button onClick={check}>Check</button>
+      <p>{message}</p>
+    </div>
+  );
+} 
+function AllQ(props){
+  const checkAll = () => {
+      qArr.forEach((props.qArr.q, props.qArr.a))
+  }
+  return (<div>
+    <ol>
+    {props.qArr.map((qna, i) => (<li key={i}><SimpleQ q={qna.q} a={qna.a}/></li>)) }
+  </ol>
+  <button onClick={checkAll}>Check all</button>
+  </div>
+  )
+}
+
+const qnaArr = [{q: "2 + 3", a: "5"}, 
+  {q: "2 * 3", a: "6"}, 
+  {q: "what the color of the sky", a: "blue"}, 
+  {q: "what the color of the sun", a: "yellow"}, 
+  {q: "what the color of the grass", a: "green"}, 
+  {q: "what the color of the sea", a: "blue"}]
 
 class App extends react.PureComponent {
   render() {
@@ -34,6 +70,7 @@ class App extends react.PureComponent {
         <h1>My App</h1>
         <NewCounter start="5" jump="3" />
         <ColorSqure/>
+        <AllQ qArr={qnaArr}/>
       </div>
     )
   }
