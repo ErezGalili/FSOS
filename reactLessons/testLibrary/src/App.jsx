67e1be1d939f55d29e47a2c5b76df3dc9a4e7725
ErezGalili <galiliere13@gmail.com>
2024-12-25 11:49:41 +0200

    25.12 update 2

--- reactLessons/testLibrary/src/App.jsx
+++ reactLessons/testLibrary/src/App.jsx
@@ -1,23 +1,62 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-function Books() {
-  const [book, setBook] = useState('')
-  
+function AddBook({ onAdd }) {
+  const [book, setBook] = useState('');
+  const [img, setImg] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const addBook = () => {
+    if (book && img && author) {
+      onAdd({ book, img, author });
+      setBook('');
+      setImg('');
+      setAuthor('');
+    }
+  };
+
   return (
-    <div>
-      <h1>Books</h1>
+    <div id="addBook">
+      <h1>The library</h1>
+      <h3>Enter book name</h3>
+      <input type="text" value={book} onChange={(e) => setBook(e.target.value)} />
+      <h3>Enter img url</h3>
+      <input type="text" value={img} onChange={(e) => setImg(e.target.value)} />
+      <h3>Enter author name</h3>
+      <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} />
+      <button className='addBtn' onClick={addBook}>Add book</button>
     </div>
-  )
+  );
+}
+
+function Books({ books }) {
+  return (
+    <div id="books">
+      {books.map((b, index) => (
+        <div key={index} id="book">
+          <h3>{b.book}</h3>
+          <img src={b.img} alt={b.book} />
+          <p>Author: {b.author}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const handleAddBook = (newBook) => {
+    setBooks([...books, newBook]);
+  };
 
   return (
     <>
-      <Books />
+      <AddBook onAdd={handleAddBook} />
+      <Books books={books} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
